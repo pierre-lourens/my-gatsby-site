1b4e63f4327c4d93ffe9bdc5b641eeecea277387
Pierre Lourens <plourens3@gmail.com>
2022-03-02 19:41:56 -0800

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle='About Me'>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+        I love my dog. I have created apps with React before, but it was a while
+        ago, and this Gatsby tutorial is a good refresher.
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
